42bd6e2596671e9fe195f6a729aea290124dda6e
Rocco Totaro <cinema.totaro@gmail.com>
2025-09-14 17:28:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -270,4 +270,5 @@ app = create_app()
 
 # Avvio locale
 if __name__ == "__main__":
-    port = int(os.environ.get("POR
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
